b327ee04951bd2a3c9db365e8861afb74c4457f4
Rebecca Allen and Zach Hedgepeth <rebecca@>
2016-08-09 11:45:13 -0400

    add buttons to switch color theme

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,24 @@ $(document).ready(function() {
   $(".clickable1").click(function() {
     $("#sandcastle1").slideToggle();
   });
+
   $(".clickable2").click(function() {
     $("#sandcastle2").slideToggle();
   });
+
   $(".clickable3").click(function() {
     $("#sandcastle3").slideToggle();
   });
+
+  $("button#light").click(function(){
+    $("body").removeClass();
+    $("body").addClass("light");
+  });
+
+  $("button#dark").click(function(){
+    $("body").removeClass();
+    $("body").addClass("dark");
+  });
+
+
 });

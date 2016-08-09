de9bb9d4352b426bec361a6f69b2ca2905fa624e
Rebecca Allen and Zach Hedgepeth <rebecca+zach@>
2016-08-09 11:02:33 -0400

    create site about sandcastles, with css styling and js/jquery operations

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".clickable1").click(function() {
+    $("#sandcastle1").slideToggle();
+  });
+  $(".clickable2").click(function() {
+    $("#sandcastle2").slideToggle();
+  });
+  $(".clickable3").click(function() {
+    $("#sandcastle3").slideToggle();
+  });
+});
